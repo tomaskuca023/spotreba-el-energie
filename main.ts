0134b485024fde7d545ae692a0a3ff8346ba2b2a
tomaskuca023 <146720075+tomaskuca023@users.noreply.github.com>
2024-02-16 11:33:23 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,15 @@
+type Odber = {
+    cas: number; //milisekunda
+    pricon: number; // watt
+}
+
+let data: Array<Odber> = [
+    {cas:100, pricon:35000},
+    {cas:200, pricon:40000},
+    {cas:300, pricon:45000}, //6.7 kc / kilowatt
+    {cas:400, pricon:50000},
+    {cas:500, pricon:55000}
+]
+for (let i = 0; i < data.length; i++) {
+    
+}
